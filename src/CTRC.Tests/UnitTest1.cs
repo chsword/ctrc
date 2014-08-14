9f1a93c442857dd855a802baaab6eae0ab28a943
Zou Jian <chsword@126.com>
2014-08-14 14:40:38 +0800

    init

---
+++ src/CTRC.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CTRC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
