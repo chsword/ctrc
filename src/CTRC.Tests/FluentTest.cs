168ea4204772be6da2f3e07e153d95edd7df6128
ZouJian <chsword@qq.com>
2014-09-21 12:04:28 +0800

    add fluent test

---
+++ src/CTRC.Tests/FluentTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CTRC.Tests
+{
+    [TestClass]
+    public class FluentTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
